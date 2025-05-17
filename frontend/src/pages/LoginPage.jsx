763780942c8750c6584d6833022df95b808131d6
João Parro <jpedroparro@gmail.com>
2025-05-17 14:15:04 -0300

    Substitui a pasta frontend pela versão local

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+// frontend/src/pages/LoginPage.jsx
+import LoginForm from '../components/LoginForm';
+
+export default function LoginPage() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <LoginForm />
+    </div>
+  );
+}
